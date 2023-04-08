a586f748146f698e2bd5d401b7f55886b042083f
myxl11 <128257230+myxl11@users.noreply.github.com>
2023-04-08 08:33:37 +0700

    Update otp.py

--- otp.py
+++ otp.py
@@ -46,6 +46,14 @@ def send_paginated_country_list(chat_id, message_id=None, offset=0, step=10):
         message_id = sent_message.message_id
     return message_id
 
+def get_price_list(api_key, country_id):
+    url = f'https://kodeotp.com/api?api_key={api_key}&action=get_service&country_id={country_id}&type=regular'
+    response = requests.get(url)
+    data = json.loads(response.text)
+    price_list = data['data']
+    formatted_price_list = "\n".join([f"{service['service_name']} - {service['cost']} (Stok: {service['count']})" for service in price_list])
+    return formatted_price_list
+
 @bot.message_handler(commands=['start', 'help'])
 def send_welcome(message):
     bot.reply_to(message, "Selamat datang! Untuk mengecek saldo, ketik /saldo. Untuk melihat daftar negara, ketik /listnegara.")
@@ -72,5 +80,14 @@ def handle_navigation(call):
     offset = int(offset)
     bot.answer_callback_query(call.id)
     send_paginated_country_list(call.message.chat.id, message_id=call.message.message_id, offset=offset)
-
+@bot.message_handler(commands=['listharga'])
+def send_listharga(message):
+    try:
+        country_id = message.text.split()[1]  # Ambil parameter country_id dari pesan
+        price_list = get_price_list('73de2d7580ec0ed58df2795ebfd1703c', country_id)
+        bot.reply_to(message, f"Daftar Harga:\n{price_list}")
+    except IndexError:
+        bot.reply_to(message, "Format perintah salah. Gunakan format: /listharga <country_id>")
+    except Exception as e:
+        bot.reply_to(message, f"Terjadi kesalahan saat mengambil daftar harga. Silakan coba lagi nanti.\nError: {e}")
 bot.polling()
