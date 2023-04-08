815d588bdb381177a7a279054e9bb76c48b5ce33
myxl11 <128257230+myxl11@users.noreply.github.com>
2023-04-08 08:38:39 +0700

    Update otp.py

--- otp.py
+++ otp.py
@@ -13,7 +13,7 @@ def get_balance(api_key):
     saldo = int(float(data['data']['balance']))
     formatted_saldo = f"Rp {saldo:,}".replace(",", ".")
     return formatted_saldo
-
+#list negara
 def get_country_list(api_key):
     url = f'https://kodeotp.com/api?api_key={api_key}&action=country'
     response = requests
@@ -45,14 +45,35 @@ def send_paginated_country_list(chat_id, message_id=None, offset=0, step=10):
         sent_message = bot.send_message(chat_id, f"Daftar Negara:\n{formatted_list}", reply_markup=markup)
         message_id = sent_message.message_id
     return message_id
-
-def get_price_list(api_key, country_id):
+#list harga
+def get_price_list(api_key, country_id, offset=0, step=10):
     url = f'https://kodeotp.com/api?api_key={api_key}&action=get_service&country_id={country_id}&type=regular'
     response = requests.get(url)
     data = json.loads(response.text)
     price_list = data['data']
-    formatted_price_list = "\n".join([f"{service['service_name']} - {service['cost']} (Stok: {service['count']})" for service in price_list])
-    return formatted_price_list
+    paginated_list = price_list[offset:offset + step]
+    formatted_price_list = "\n".join([f"{service['service_name']} - {service['cost']} (Stok: {service['count']})" for service in paginated_list])
+    return formatted_price_list, len(price_list)
+
+def send_paginated_price_list(chat_id, country_id, message_id=None, offset=0, step=10):
+    api_key = '73de2d7580ec0ed58df2795ebfd1703c'
+    price_list, total_count = get_price_list(api_key, country_id, offset, step)
+
+    if not price_list:
+        return
+
+    markup = telebot.types.InlineKeyboardMarkup()
+    if offset > 0:
+        markup.add(telebot.types.InlineKeyboardButton("⬅️ Kembali", callback_data=f"price_prev_{country_id}_{offset - step}"))
+    if total_count > offset + step:
+        markup.add(telebot.types.InlineKeyboardButton("Lanjutkan ➡️", callback_data=f"price_next_{country_id}_{offset + step}"))
+
+    if message_id:
+        bot.edit_message_text(f"Daftar Harga:\n{price_list}", chat_id, message_id, reply_markup=markup)
+    else:
+        sent_message = bot.send_message(chat_id, f"Daftar Harga:\n{price_list}", reply_markup=markup)
+        message_id = sent_message.message_id
+    return message_id
 
 @bot.message_handler(commands=['start', 'help'])
 def send_welcome(message):
@@ -84,10 +105,15 @@ def handle_navigation(call):
 def send_listharga(message):
     try:
         country_id = message.text.split()[1]  # Ambil parameter country_id dari pesan
-        price_list = get_price_list('73de2d7580ec0ed58df2795ebfd1703c', country_id)
-        bot.reply_to(message, f"Daftar Harga:\n{price_list}")
+        send_paginated_price_list(message.chat.id, country_id)
     except IndexError:
         bot.reply_to(message, "Format perintah salah. Gunakan format: /listharga <country_id>")
     except Exception as e:
         bot.reply_to(message, f"Terjadi kesalahan saat mengambil daftar harga. Silakan coba lagi nanti.\nError: {e}")
+@bot.callback_query_handler(func=lambda call: call.data.startswith("price_prev_") or call.data.startswith("price_next_"))
+def handle_price_navigation(call):
+    action, country_id, offset = call.data.split("_")
+    offset = int(offset)
+    bot.answer_callback_query(call.id)
+    send_paginated_price_list(call.message.chat.id, country_id, message_id=call.message.message_id, offset=offset)
 bot.polling()
